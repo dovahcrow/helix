usagi-flow/evil-helix

Squashed commit of the following: (#3)
commit cdc53f42e8e9eeb19f7411bd3d026d75188cd8f5
Author: Jesse Luehrs <doy@tozt.net>
Date:   Sun Sep 17 13:25:52 2023 -0400

    add a separate "helix:" modeline for setting helix-specific config

    for example, if the language name differs between vim and helix

commit 01554579e9ffdb2922b68bc55a42c36be76d3f4f
Author: Jesse Luehrs <doy@tozt.net>
Date:   Sun Sep 17 12:35:57 2023 -0400

    add basic modeline support

    currently only supports setting language, indent style, and line endings

helix-view/src/document.rs
@@ -31,6 +31,7 @@ use helix_core::{
     history::{History, State, UndoKind},
     indent::{auto_detect_indent_style, IndentStyle},
     line_ending::auto_detect_line_ending,
+    modeline::Modeline,
     syntax::{self, LanguageConfiguration},
     ChangeSet, Diagnostic, LineEnding, Range, Rope, RopeBuilder, Selection, Syntax, Transaction,
 };
@@ -178,6 +179,8 @@ pub struct Document {
     pub focused_at: std::time::Instant,
 
     pub readonly: bool,
+
+    modeline: Modeline,
 }
 
 /// Inlay hints for a single `(Document, View)` combo.
@@ -637,6 +640,7 @@ impl Document {
         let line_ending = config.load().default_line_ending.into();
         let changes = ChangeSet::new(text.slice(..));
         let old_state = None;
+        let modeline = Modeline::parse(text.slice(..));
 
         Self {
             id: DocumentId::default(),
@@ -668,6 +672,7 @@ impl Document {
             focused_at: std::time::Instant::now(),
             readonly: false,
             jump_labels: HashMap::new(),
+            modeline,
         }
     }
 
@@ -996,21 +1001,35 @@ impl Document {
         &self,
         config_loader: &syntax::Loader,
     ) -> Option<Arc<helix_core::syntax::LanguageConfiguration>> {
-        config_loader
-            .language_config_for_file_name(self.path.as_ref()?)
-            .or_else(|| config_loader.language_config_for_shebang(self.text().slice(..)))
+        self.modeline
+            .language()
+            .and_then(|language| config_loader.language_config_for_language_id(language))
+            .or_else(|| {
+                config_loader
+                    .language_config_for_file_name(self.path.as_ref()?)
+                    .or_else(|| config_loader.language_config_for_shebang(self.text().slice(..)))
+            })
     }
 
     /// Detect the indentation used in the file, or otherwise defaults to the language indentation
     /// configured in `languages.toml`, with a fallback to tabs if it isn't specified. Line ending
     /// is likewise auto-detected, and will remain unchanged if no line endings were detected.
     pub fn detect_indent_and_line_ending(&mut self) {
-        self.indent_style = auto_detect_indent_style(&self.text).unwrap_or_else(|| {
-            self.language_config()
-                .and_then(|config| config.indent.as_ref())
-                .map_or(DEFAULT_INDENT, |config| IndentStyle::from_str(&config.unit))
-        });
-        if let Some(line_ending) = auto_detect_line_ending(&self.text) {
+        self.indent_style = self
+            .modeline
+            .indent_style()
+            .or_else(|| auto_detect_indent_style(&self.text))
+            .unwrap_or_else(|| {
+                self.language_config()
+                    .and_then(|config| config.indent.as_ref())
+                    .map_or(DEFAULT_INDENT, |config| IndentStyle::from_str(&config.unit))
+            });
+
+        if let Some(line_ending) = self
+            .modeline
+            .line_ending()
+            .or_else(|| auto_detect_line_ending(&self.text))
+        {
             self.line_ending = line_ending;
         }
     }

dovahcrow/helix

helix-view/src/document.rs
@@ -39,6 +39,7 @@ use helix_core::{
     history::{History, State, UndoKind},
     indent::{auto_detect_indent_style, IndentStyle},
     line_ending::auto_detect_line_ending,
+    modeline::Modeline,
     syntax::{self, config::LanguageConfiguration},
     ChangeSet, Diagnostic, LineEnding, Range, Rope, RopeBuilder, Selection, Syntax, Transaction,
 };
@@ -203,6 +204,8 @@ pub struct Document {
     pub focused_at: std::time::Instant,
 
     pub readonly: bool,
+
+    modeline: Modeline,
 
     /// Annotations for LSP document color swatches
     pub color_swatches: Option<DocumentColorSwatches>,
@@ -690,6 +693,7 @@ impl Document {
         let line_ending = config.load().default_line_ending.into();
         let changes = ChangeSet::new(text.slice(..));
         let old_state = None;
+        let modeline = Modeline::parse(text.slice(..));
 
         Self {
             id: DocumentId::default(),
@@ -725,6 +729,7 @@ impl Document {
             focused_at: std::time::Instant::now(),
             readonly: false,
             jump_labels: HashMap::new(),
+            modeline,
             color_swatches: None,
             color_swatch_controller: TaskController::new(),
             syn_loader,
@@ -1144,9 +1149,15 @@ impl Document {
         &self,
         loader: &syntax::Loader,
     ) -> Option<Arc<syntax::config::LanguageConfiguration>> {
-        let language = loader
-            .language_for_filename(self.path.as_ref()?)
-            .or_else(|| loader.language_for_shebang(self.text().slice(..)))?;
+        let language = self
+            .modeline
+            .language()
+            .and_then(|language| loader.language_for_name(language))
+            .or_else(|| {
+                loader
+                    .language_for_filename(self.path.as_ref()?)
+                    .or_else(|| loader.language_for_shebang(self.text().slice(..)))
+            })?;
 
         Some(loader.language(language).config().clone())
     }
@@ -1155,7 +1166,9 @@ impl Document {
     /// configured in `languages.toml`, with a fallback to tabs if it isn't specified. Line ending
     /// is likewise auto-detected, and will remain unchanged if no line endings were detected.
     pub fn detect_indent_and_line_ending(&mut self) {
-        self.indent_style = if let Some(indent_style) = self.editor_config.indent_style {
+        self.indent_style = if let Some(indent_style) = self.modeline.indent_style() {
+            indent_style
+        } else if let Some(indent_style) = self.editor_config.indent_style {
             indent_style
         } else {
             auto_detect_indent_style(&self.text).unwrap_or_else(|| {